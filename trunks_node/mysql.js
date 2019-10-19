b72beef4e395cb701237adef00fdb29339ca4156
Trunks <18742509891@163.com>
2019-10-19 16:29:01 +0800

    后端接口开发

--- trunks_node/mysql.js
+++ trunks_node/mysql.js
@@ -1,48 +1,61 @@
 //数据库类
 export class _mysql {
-    constructor(getdata) {
-       this.mysql = require('mysql');
-       this.getData = getdata;
-       this.connection = this.mysql.createConnection({
-          host: 'localhost',
-          user: 'root',  //用户名
-          password: '',   //密码
-          database: 'Trunks',
-          port: '3306'     //端口号
-       });
-    }
-    connect() {
-       this.connection.connect(function (err) {
-          if (err) {
-             console.log('---:' + err);
-             return;
-          }
-          console.log('连接succeed');
-       });
-    }
-    //知识点上传
-    addBlog(callback) {
-       this.connection.query('insert into blog_data (user_id,title,article,is_share,sort,sort_id) values (?,?,?,?,?,?)',
-          [this.getData.user_id, this.getData.title, this.getData.article, this.getData.is_share, this.getData.sort, this.getData.sort_id],
-          (err, rs, fields) => {
-             if (err) {
-                return callback(err, rs, fields)
-             } else {
-                this.connection.query('select * from blog_data where id = LAST_INSERT_ID()', (err, blog, fields) => {
-                   return callback(err, blog, fields)
-                })
-             }
-          })
-    }
-    //关闭数据库
-    close() {
-       this.connection.end(function (err) {
-          if (err) {
-             console.log('---:' + err);
-             return;
-          }
-          console.log('关闭succeed');
-       });
-    }
- 
- }
+   constructor(getdata) {
+      this.mysql = require('mysql');
+      this.getData = getdata;
+      this.connection = this.mysql.createConnection({
+         host: 'localhost',
+         user: 'root',  //用户名
+         password: '',   //密码
+         database: 'Trunks',
+         port: '3306'     //端口号
+      });
+   }
+   connect() {
+      this.connection.connect(function (err) {
+         let time = new Date()
+         if (err) {
+
+            console.log('Err:' + err + '---' + time);
+            return;
+         }
+
+         console.log('连接succeed   ----' + time);
+      });
+   }
+   //登录
+   getUserInfo(callback) {
+      this.connection.query('select * from user_info where user_name = ? and pass_word = ?', [this.getData.userName, this.getData.passWord], (err, rs, fields) => {
+         if (err) {
+            return callback(err, rs, fields)
+         } else {
+            return callback(err, blog, fields)
+         }
+      })
+   }
+   //注册
+   addUser(callback) {
+      this.connection.query('insert into user_info (user_name,name,pass_word,email) value(?,?,?,?)', [this.getData.userName, this.getData.name,this.getData.passWord,this.getData.email], (err, rs, fields) => {
+         if (err) {
+            return callback(err, rs, fields)
+         } else {
+            return callback(err, blog, fields)
+         }
+      })
+   }
+   //新增博客
+   addBlog(callback){
+      
+   }
+   //关闭数据库
+   close() {
+      this.connection.end(function (err) {
+         if (err) {
+            console.log('---:' + err);
+            return;
+         }
+         console.log('关闭succeed');
+      });
+   }
+
+}
