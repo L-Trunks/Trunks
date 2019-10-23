7c731033b6be906b201ccf4cfe72548085f4b4f5
Trunks <18742509891@163.com>
2019-10-23 16:36:33 +0800

    接口开发

--- trunks_node/mysql.js
+++ trunks_node/mysql.js
@@ -35,7 +35,7 @@ export class _mysql {
    }
    //注册
    addUser(callback) {
-      this.connection.query('insert into user_info (user_name,name,pass_word,email) value(?,?,?,?)', [this.getData.userName, this.getData.name,this.getData.passWord,this.getData.email], (err, rs, fields) => {
+      this.connection.query('insert into user_info (user_name,name,pass_word,email) value(?,?,?,?)', [this.getData.userName, this.getData.name, this.getData.passWord, this.getData.email], (err, rs, fields) => {
          if (err) {
             return callback(err, rs, fields)
          } else {
@@ -44,9 +44,78 @@ export class _mysql {
       })
    }
    //新增博客
-   addBlog(callback){
-      
+   addBlog(callback) {
+      this.connection.query('insert into blog_info (title,article,sign,is_original) value(?,?,?,?)', [this.getData.title, this.getData.article, this.getData.sign, this.getData.isOriginal], (err, rs, fields) => {
+         if (err) {
+            return callback(err, rs, fields)
+         } else {
+            return callback(err, blog, fields)
+         }
+      })
    }
+   //获取博客列表
+   getBlog(callback) {
+      this.connection.query('select * from blog_info', [], (err, rs, fields) => {
+         if (err) {
+            return callback(err, rs, fields)
+         } else {
+            return callback(err, blog, fields)
+         }
+      })
+   }
+   //删除博客
+   deleteBlog(callback) {
+      this.connection.query('delete from blog_info where id = ?', [this.getData.id], (err, rs, fields) => {
+         if (err) {
+            return callback(err, rs, fields)
+         } else {
+            return callback(err, blog, fields)
+         }
+      })
+   }
+   //修改博客
+   updateBlog(callback) {
+      this.connection.query('update blog_info set title = ?,article = ?,sign = ?,is_original = ? where id = ?',
+         [this.getData.title, this.getData.article, this.getData.sign, this.getData.isOriginal, this.getData.id], (err, rs, fields) => {
+            if (err) {
+               return callback(err, rs, fields)
+            } else {
+               return callback(err, blog, fields)
+            }
+         })
+   }
+   //浏览量
+   addLookCount(callback){
+      this.connection.query('update blog_info set look_count = look_count+1 where id = ?',
+         [this.getData.lookCount,this.getData.id], (err, rs, fields) => {
+            if (err) {
+               return callback(err, rs, fields)
+            } else {
+               return callback(err, blog, fields)
+            }
+         })
+   }
+   //获取标签列表
+   getSign(callback) {
+      this.connection.query('select * from sign_info', [], (err, rs, fields) => {
+         if (err) {
+            return callback(err, rs, fields)
+         } else {
+            return callback(err, blog, fields)
+         }
+      })
+   }
+   //添加标签
+   addSign(callback){
+      this.connection.query('insert into sign_info (sign) value (?)', [this.getData.sign], (err, rs, fields) => {
+         if (err) {
+            return callback(err, rs, fields)
+         } else {
+            return callback(err, blog, fields)
+         }
+      })
+   }
+   
    //关闭数据库
    close() {
       this.connection.end(function (err) {
