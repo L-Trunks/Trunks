bd7f7d517d63764050f8ec6525ef53e0dd01f556
Trunks <18742509891@163.com>
2019-11-05 14:53:08 +0800

    登录注册，博客相关接口开发

--- trunks_node/app.js
+++ trunks_node/app.js
@@ -18,35 +18,190 @@ app.all('*', function (req, res, next) {
 
 //用户操作接口
 
+//登录
 
-//用户名列表，用于提示用户该用户名是否存在
-app.post('/api/user/user_name_list', (req, res, next) => {
-   let mysql = new _mysql();
-
-   mysql.getNameList((err, list, fields) => {
+app.post('/trunks/user/login', (req, res, next) => {
+   let getdata = req.body.params
+   let mysql = new _mysql(getdata)
+   mysql.getUserInfo((err, list, fields) => {
       if (err) {
-         res.json({ res: 1, message: '请求失败' })
+         res.json({ res: 1, message: '登录失败' })
          next(err);
       } else {
-         // let get_data = [];
-         // for (let i = 0; i < list.length; i++) {
-         //    get_data.push(list[i].name)
-         // }
-         console.log('用户名列表请求成功')
+         let date = new Date()
+         console.log('登录成功 -----' + date)
          res.status(200);
-         res.json({ res: 0, message: '请求处理成功', data: list })
+         res.json({ res: 0, message: '登录成功', data: list })
+      }
+      mysql.close()
+   })
+})
+
+//注册
+
+app.post('/trunks/user/register', function (req, res, next) {
+   let getdata = req.query.params
+   let mysql = new _mysql(getdata)
+
+   mysql.addUser(function (error, data, fields) {
+      if (error) {
+         res.json({ res: 1, message: '注册失败' })
+         next(error);
+      } else {
+         let date = new Date()
+         console.log("log:" + "注册成功-----" + date);
+         res.status(200)
+         res.json({ res: 0, message: '注册成功', data: data })
+      }
+      mysql.close()
+   })
+})
+
+//新增博客
+
+app.post('/trunks/blog/add_blog', function (req, res, next) {
+   let getdata = req.query.params
+   let mysql = new _mysql(getdata)
+   mysql.addBlog(function (error, data, fields) {
+      if (error) {
+         res.json({ res: 1, message: '新增博客失败' })
+         next(error);
+      } else {
+         let date = new Date()
+         console.log("log:" + "新增博客成功-----" + date);
+         res.status(200)
+         res.json({ res: 0, message: '新增博客成功', data: data })
       }
       mysql.close()
    })
 
 })
 
+//获取博客列表
+
+app.get('/trunks/blog/get_blog_list', function (req, res, next) {
+   // let getdata = JSON.parse(req.query.params)
+   let mysql = new _mysql(getdata)
+   mysql.getBlog(function (error, data, fields) {
+      if (error) {
+         res.json({ res: 1, message: '获取博客列表失败' })
+         next(error);
+      } else {
+         let date = new Date()
+         console.log("log:" + "获取博客列表成功-----" + date);
+         res.status(200)
+         res.json({ res: 0, message: '获取博客列表成功', data: data })
+      }
+      mysql.close()
+   })
+
+})
+
+//删除博客
+
+app.post('/trunks/blog/delete_blog', function (req, res, next) {
+   let getdata = req.query.params
+   let mysql = new _mysql(getdata)
+   mysql.deleteBlog(function (error, data, fields) {
+      if (error) {
+         res.json({ res: 1, message: '删除博客失败' })
+         next(error);
+      } else {
+         let date = new Date()
+         console.log("log:" + "删除博客成功-----" + date);
+         res.status(200)
+         res.json({ res: 0, message: '删除博客成功', data: data })
+      }
+      mysql.close()
+   })
+
+})
+
+//修改博客
+
+app.post('/trunks/blog/update_blog', function (req, res, next) {
+   let getdata = req.query.params
+   let mysql = new _mysql(getdata)
+   mysql.updateBlog(function (error, data, fields) {
+      if (error) {
+         res.json({ res: 1, message: '修改博客失败' })
+         next(error);
+      } else {
+         let date = new Date()
+         console.log("log:" + "修改博客成功-----" + date);
+         res.status(200)
+         res.json({ res: 0, message: '修改博客成功', data: data })
+      }
+      mysql.close()
+   })
+
+})
+
+//修改博客
+
+app.get('/trunks/blog/add_look_count', function (req, res, next) {
+   let getdata =　JSON.parse(req.query.params) 
+   let mysql = new _mysql(getdata)
+   mysql.addLookCount(function (error, data, fields) {
+      if (error) {
+         res.json({ res: 1, message: '浏览量设置失败' })
+         next(error);
+      } else {
+         let date = new Date()
+         console.log("log:" + "浏览量＋１-----" + date);
+         res.status(200)
+         res.json({ res: 0, message: '浏览量＋１', data: data })
+      }
+      mysql.close()
+   })
+
+})
+
+//获取标签列表
+
+app.get('/trunks/blog/sign_list', function (req, res, next) {
+   let getdata =　JSON.parse(req.query.params) 
+   let mysql = new _mysql(getdata)
+   mysql.getSign(function (error, data, fields) {
+      if (error) {
+         res.json({ res: 1, message: '标签列表获取成功' })
+         next(error);
+      } else {
+         let date = new Date()
+         console.log("log:" + "标签列表获取成功-----" + date);
+         res.status(200)
+         res.json({ res: 0, message: '标签列表获取成功', data: data })
+      }
+      mysql.close()
+   })
+
+})
+
+//添加标签
+
+app.post('/trunks/blog/add_sign', function (req, res, next) {
+   let getdata = req.query.params
+   let mysql = new _mysql(getdata)
+   mysql.addSign(function (error, data, fields) {
+      if (error) {
+         res.json({ res: 1, message: '添加标签失败' })
+         next(error);
+      } else {
+         let date = new Date()
+         console.log("log:" + "添加标签成功-----" + date);
+         res.status(200)
+         res.json({ res: 0, message: '添加标签成功', data: data })
+      }
+      mysql.close()
+   })
+
+})
 //配置服务端口
 const server = app.listen(7788, function () {
 
-    const host = server.address().address;
- 
-    const port = server.address().port;
- 
-    console.log('Example app listening at http://%s:%s', host, port);
- })
+   const host = server.address().address;
+
+   const port = server.address().port;
+
+   console.log('Example app listening at http://%s:%s', host, port);
+})
