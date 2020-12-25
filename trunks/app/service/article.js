ed8121bd975fbe2e68cebd6e4b189ac85d8aabd0
L-Trunks <18742509891@163.com>
2020-12-25 08:37:07 +0800

    eslint格式修改...

--- trunks/app/service/article.js
+++ trunks/app/service/article.js
@@ -4,13 +4,14 @@ const Service = require('egg').Service;
 
 class ArticleService extends Service {
   /**
-       * 根据ID获取单个项目
-       */
-  async getProjectById() {
+    * @param {Object} params
+    * 根据ID获取单个项目
+  */
+  async getProjectById(params) {
     const { ctx, app } = this;
     try {
       const results = await ctx.model.Article.find({ // Article为modal/article.js里面命名的名字
-        _id: app.mongoose.Types.ObjectId('5da034149b6e823ca2ea809d'),
+        _id: app.mongoose.Types.ObjectId(params._id),
       });
       return results;
     } catch (err) {
